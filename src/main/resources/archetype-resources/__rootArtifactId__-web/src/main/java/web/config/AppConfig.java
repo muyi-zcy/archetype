bdbfc7e4e96063a8509d0298177177ed94dd7ec6
muyi <zcy_nemo@aliyun.com>
2021-10-19 00:25:55 +0800

    init commit

---
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/config/AppConfig.java
@@ -0,0 +1,25 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.web.config;
+
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * description: AppConfig
+ * date: 2021/10/12 23:33
+ * author: muyi
+ * version: 1.0
+ */
+
+@SpringBootApplication(scanBasePackages = "${package}.*")
+@MapperScan("${package}.core.dao")
+@NacosPropertySource(dataId = "${parentArtifactId}.yaml",groupId = "${artifactId}-dev", autoRefreshed = true)
+public class AppConfig {
+    public static void main(String[] args) {
+        SpringApplication.run(AppConfig.class);
+    }
+}
---
+++ src/main/resources/archetype-resources/__rootArtifactId__-web/src/main/java/web/controller/OkController.java
@@ -0,0 +1,38 @@
+#set( $symbol_pound = '#' )
+#set( $symbol_dollar = '$' )
+#set( $symbol_escape = '\' )
+package ${package}.web.controller;
+
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import ${package}.base.start.properties.SystemInfoProperties;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+
+/**
+ * description: 服务健康检查
+ * author: muyi
+ * version: 1.0
+ */
+
+@RestController
+public class OkController {
+
+    private static final Logger logger = LoggerFactory.getLogger(OkController.class);
+
+    @Autowired
+    private SystemInfoProperties systemInfoProperties;
+
+
+    @GetMapping("/ok")
+    public Object ok(HttpServletRequest httpRequest) {
+        logger.info(systemInfoProperties.getSystemName());
+        logger.info(systemInfoProperties.getProfileActive());
+        logger.info(systemInfoProperties.getRunTime());
+        return "ok";
+    }
+}
